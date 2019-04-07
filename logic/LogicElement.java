ccbc673a30fbc174e1e4d33f4acbc8e6f4d0c047
Jonah Pierce <fellowhashbrown@gmail.com>
2019-04-06 20:51:01 -0600

    Create LogicElement.java

---
+++ logic/LogicElement.java
@@ -0,0 +1,18 @@
+package logic;
+
+import java.util.LinkedList;
+import util.Evaluation;
+import util.TruthValue;
+
+/**
+ * An interface used to allow for flexibility inside the parseExpression method in {@link LogicTree}.
+ */
+public interface LogicElement {
+
+    public int getOperatorType();
+    public boolean hasNot();
+    public String getNot();
+    public LinkedList<Evaluation> getTruthValues(LinkedList<TruthValue> truthValues);
+    public boolean evaluate(TruthValue truthValue);
+
+}
