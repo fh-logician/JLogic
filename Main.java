d0c2c772b1c823de08d30a52516ce787a5c9b47d
Jonah Pierce <fellowhashbrown@gmail.com>
2019-05-15 01:38:14 -0600

    Added some comments

--- Main.java
+++ Main.java
@@ -4,12 +4,20 @@ public class Main {
     public static void main(String[] args) {
     
         String[] expressions = new String[] {
+			
+			// Pseudo Examples
             "a or (a and b)",
 			"(not a and not b) or (not c or not b)",
+			
+			// Logic Examples
             "~(a v b)",
             "(a ^ b) ^ c",
+			
+			// Code Examples
 			"(a || b) && (!d || c)",
 			"(p && q && r) || (p && q && !r) || (p && !q && !r)",
+			
+			// Boolean Algebra Examples
 			"(a*b*c) + (a*-b*-d) + (a*b*-c) + (a*b*d)",
 			"(a+b+c)*(a+-b+-d)*(a+b+-c)*(a+b+d)"
         };
