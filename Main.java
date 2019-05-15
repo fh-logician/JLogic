51743aa410d7971feb0fe85189d643219bbe4420
Jonah Pierce <fellowhashbrown@gmail.com>
2019-05-15 01:30:23 -0600

    Added examples of different operator types

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ public class Main {
     public static void main(String[] args) {
     
         String[] expressions = new String[] {
-            "a ^ b",
+            "a or (a and b)",
+			"(not a and not b) or (not c or not b)",
             "~(a v b)",
             "(a ^ b) ^ c",
-            "a ^ b ^ c",
-            "a ^ (b ^ c)",
-            "~(a v b) ^ c",
-            "~(a v b) ^ ~(a v c)",
-            "~(~(a v b) ^ ~(a v c))",
-            "a v b v c v d"
+			"(a || b) && (!d || c)",
+			"(p && q && r) || (p && q && !r) || (p && !q && !r)",
+			"(a*b*c) + (a*-b*-d) + (a*b*-c) + (a*b*d)",
+			"(a+b+c)*(a+-b+-d)*(a+b+-c)*(a+b+d)"
         };
 
         for (String expression: expressions) {
             LogicTree tree = new LogicTree(expression);
             System.out.println(expression);
+            System.out.println(tree.simplify());
             tree.printTable();
             System.out.println();
         }
